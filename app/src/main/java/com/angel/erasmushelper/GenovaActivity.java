a2e600f41397412e249639f58ddc5312a0700a00
pedro5basso <pedrobasso98@gmail.com>
2020-05-03 11:59:43 +0200

    Angel: Primera versión y primeros layouts

---
+++ app/src/main/java/com/angel/erasmushelper/GenovaActivity.java
@@ -0,0 +1,17 @@
+package com.angel.erasmushelper;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GenovaActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.genova_activity);
+
+    }
+}
---
+++ app/src/main/java/com/angel/erasmushelper/Home_Activity.java
@@ -0,0 +1,39 @@
+package com.angel.erasmushelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home_Activity extends AppCompatActivity {
+
+    String TAG="MainActivity";
+    Button bttnStartGenova, bttnStartUnige, bttnStartExp, bttnStartHousing, bttnStartFinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        Log.i(TAG, "OnCreate");
+
+        bttnStartGenova = findViewById(R.id.buttonA);
+        bttnStartUnige = findViewById(R.id.buttonB);
+        bttnStartExp = findViewById(R.id.buttonC);
+        bttnStartHousing = findViewById(R.id.buttonD);
+        bttnStartFinder = findViewById(R.id.buttonE);
+
+        bttnStartGenova.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent("com.erasmushelper.GenovaActivity");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
